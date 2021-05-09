f3405c7d0c2b28e7e0daad6139ab4dfb2c9e41af
eldor <eldorodilov2@gmail.com>
2021-05-09 05:19:35 +0000

    Initial commit

---
+++ first.dart
@@ -0,0 +1,9 @@
+void first(){
+
+String firstName = "Eldor";
+int age = 26;
+double gpa = 3.5;
+bool isRegisteredVoter = false;
+
+print(firstName);
+}
---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+
+String name  = "Oscar";
+print("salom ${name}");
+
+print("isming shunaqa ekana ${name}");
+
+	print("   /|");
+	print("  / |");
+	print(" /  |");
+	print("/   |");
+	
+	print("  \n");
+	print("\n");
+	
+	first();
+
+}
